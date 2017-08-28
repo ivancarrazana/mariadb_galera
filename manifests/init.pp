aecb9c03bce3d3968d7838439477cfbf79943d30
Iván Andrés Carrazana <ivancarrazana@gmail.com>
2017-08-28 07:23:01 -0400

    Change on init.pp
    Monimal change to default .pp files header

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,5 @@
 # == Class: mariadb_galera
 #
-# Full description of class mariadb_galera here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
 # === Variables
 #
 # Here you should define a list of variables that this module would require.
